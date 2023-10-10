c5a8fa0f62e554cdc236cc9238a6f143fc436704
Rudra1808 <146955248+dubeyrudra-1808@users.noreply.github.com>
2023-10-10 21:48:46 +0530

    TTYUYURU

--- modes/Audio-Visual/script.js
+++ modes/Audio-Visual/script.js
@@ -1,32 +1,130 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const quizForm = document.getElementById("quiz-form");
-    const submitButton = document.getElementById("submit-button");
-    const resultsContainer = document.getElementById("results");
-    const correctAnswersSpan = document.getElementById("correct-answers");
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const questionContainerElement = document.getElementById('question-container');
+const scoreContainer = document.getElementById('score-container');
+const scoreDisplay = document.getElementById('score');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-    const correctAnswers = {
-        q1: "a",
-        q2: "a",
-        q3: "a"
-    };
+let shuffledQuestions, currentQuestionIndex, score;
 
-    let score = 0;
-
-    submitButton.addEventListener("click", function () {
-        let userAnswers = {};
-        let formData = new FormData(quizForm);
-
-        for (let pair of formData.entries()) {
-            userAnswers[pair[0]] = pair[1];
-        }
-
-        for (let question in userAnswers) {
-            if (userAnswers[question] === correctAnswers[question]) {
-                score++;
-            }
-        }
-
-        resultsContainer.style.display = "block";
-        correctAnswersSpan.textContent = score;
-    });
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++;
+  setNextQuestion();
 });
+
+function startGame() {
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  score = 0;
+  scoreDisplay.innerText = 'Score: 0';
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  if (currentQuestionIndex < questions.length) {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+  } else {
+    endGame();
+  }
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach(answer => {
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide');
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (correct) {
+    score++;
+    scoreDisplay.innerText = `Score: ${score}
+    Level Complete...Move to next level`;
+  }
+ 
+  if (currentQuestionIndex < questions.length - 1) {
+    nextButton.classList.remove('hide');
+  } else {
+    endGame();
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add('correct');
+  } else {
+    element.classList.add('wrong');
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
+}
+
+function endGame() {
+  questionContainerElement.classList.add('hide');
+  scoreContainer.classList.remove('hide');
+}
+
+const questions = [
+  {
+    question: 'It is related to which of the child rights?',
+    answers: [
+      { text: ' Education', correct: true },
+      { text: 'Health', correct: false },
+      { text: 'Poverty', correct: false }    ]
+  },
+  {
+    question: 'Is education right of every child?',
+    answers: [
+      { text: 'No,It is for only rich children.', correct: false },
+      { text: 'No', correct: false },
+      { text: 'No, It is for well privileged.', correct: false},
+      { text: 'Yes , It is a basic right of every child.', correct: true }
+    ]
+  },
+  {
+    question: 'All children have the right to_______',
+    answers: [
+      { text: 'best education possible.', correct: true },
+      { text: 'free will to study.', correct: false },
+      { text: 'never go to school.', correct: false }
+    ]
+  },
+  {
+    question: 'which right is concluded by this image?',
+    answers: [
+      { text: 'Right to Education', correct: true },
+      { text: 'Right to health', correct: false },
+      {text: 'Right to facility', correct:false}
+    ]
+  }
+]
